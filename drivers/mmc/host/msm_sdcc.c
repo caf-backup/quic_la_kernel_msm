d09927189692dbf8203137a9de809e2fb89e4b87
Sham Muthayyan <smuthayy@codeaurora.org>
2013-12-04 16:31:25 +0530

    msm: ipq806x: Add support for SDR104 UHS mode in DB149
    CRs Fixed: 584829
    
    Change-Id: I6dada248f96eae9f69c235356e5815e04b282c3b
    Signed-off-by: Sham Muthayyan <smuthayy@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -3962,11 +3962,17 @@ static int msmsdcc_config_cm_sdc4_dll_phase(struct msmsdcc_host *host,
 	 * Write the selected DLL clock output phase (0 ... 15)
 	 * to CDR_SELEXT bit field of MCI_DLL_CONFIG register.
 	 */
-	writel_relaxed(((readl_relaxed(host->base + MCI_DLL_CONFIG)
-			& ~(0xF << 20))
-			| (grey_coded_phase_table[phase] << 20)),
-			host->base + MCI_DLL_CONFIG);
-
+	if (gpio_is_valid(host->plat->uhs_gpio)) {
+		writel_relaxed(((readl_relaxed(host->base + MCI_DLL_CONFIG)
+						& ~(0xF << 20))
+					| 8),
+				host->base + MCI_DLL_CONFIG);
+	} else {
+		writel_relaxed(((readl_relaxed(host->base + MCI_DLL_CONFIG)
+						& ~(0xF << 20))
+					| (grey_coded_phase_table[phase] << 20)),
+				host->base + MCI_DLL_CONFIG);
+	}
 	/* Set CK_OUT_EN bit of MCI_DLL_CONFIG register to 1. */
 	writel_relaxed((readl_relaxed(host->base + MCI_DLL_CONFIG)
 			| MCI_CK_OUT_EN), host->base + MCI_DLL_CONFIG);
@@ -3978,7 +3984,12 @@ static int msmsdcc_config_cm_sdc4_dll_phase(struct msmsdcc_host *host,
 
 	config = readl_relaxed(host->base + MCI_DLL_CONFIG);
 	config |= MCI_CDR_EN;
-	config &= ~MCI_CDR_EXT_EN;
+
+	if (gpio_is_valid(host->plat->uhs_gpio)) {
+		config |= MCI_CDR_EXT_EN;
+	} else {
+		config &= ~MCI_CDR_EXT_EN;
+	}
 	writel_relaxed(config, host->base + MCI_DLL_CONFIG);
 	goto out;
 
