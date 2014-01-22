486aea8d2ca516bb35ba26dbbca5402a2bd77bd5
Mohammed Riyaz Piyarajohn <mpiyaraj@codeaurora.org>
2014-01-22 12:11:45 +0530

    ath9k: Sync the ath9k platform header file with upstream/backports.
    Change-Id: I8c0b565c71d3c24ee443a48c77930c7abc4e4abf
    CRs-Fixed: 607531
    Signed-off-by: Mohammed Riyaz Piyarajohn <mpiyaraj@codeaurora.org>

--- include/linux/ath9k_platform.h
+++ include/linux/ath9k_platform.h
@@ -22,6 +22,8 @@
 #define ATH9K_PLAT_EEP_MAX_WORDS	2048
 
 struct ath9k_platform_data {
+	const char *eeprom_name;
+
 	u16 eeprom_data[ATH9K_PLAT_EEP_MAX_WORDS];
 	u8 *macaddr;
 
@@ -29,9 +31,17 @@ struct ath9k_platform_data {
 	u32 gpio_mask;
 	u32 gpio_val;
 
+	bool endian_check;
 	bool is_clk_25mhz;
+	bool disable_2ghz;
+	bool disable_5ghz;
+	bool tx_gain_buffalo;
+
 	int (*get_mac_revision)(void);
 	int (*external_reset)(void);
+
+	int num_leds;
+	const struct gpio_led *leds;
 };
 
 #endif /* _LINUX_ATH9K_PLATFORM_H */
