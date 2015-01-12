5988bd55fa18221dd0016ed2653cf6f7625281a0
Senthilkumar N L <snlakshm@codeaurora.org>
2015-01-12 13:56:53 +0530

    qca961x: dwc3: Add USB VBUS Valid Signalling
    Enable VBUS valid for USB3 HS and SS Device mode
    
    Change-Id: I91efe3f144d5f8af6b715aa989bff111c9fae159
    Signed-off-by: Senthilkumar N L <snlakshm@codeaurora.org>

--- drivers/usb/dwc3/dwc3-qca961x.c
+++ drivers/usb/dwc3/dwc3-qca961x.c
@@ -18,31 +18,105 @@
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
-#include <linux/usb/phy.h>
+
+#define USB30_HS_PHY_CTRL	0x00000010
+#define SW_SESSVLD			(0x01 << 0x1C)
+#define UTMI_OTG_VBUS_VALID	(0x01 << 0x14)
+
+#define USB30_SS_PHY_CTRL	0x00000030
+#define LANE0_PWR_PRESENT	(0x01 << 0x18)
 
 struct dwc3_qca961x {
 	struct device *dev;
+
+	void __iomem *qscratch_base;
 
 	struct clk *master_clk;
 	struct clk *mock_utmi_clk;
 	struct clk *sleep_clk;
 	struct clk *srif_ahb_clk;
 	struct clk *srif_25m_clk;
+
+	unsigned int host;
 };
+
+/**
+ * Write register.
+ *
+ * @base - PHY base virtual address.
+ * @offset - register offset.
+ * @val - value to write.
+ */
+static inline void qscratch_write(void __iomem *base, u32 offset, u32 val)
+{
+	writel(val, base + offset);
+}
+
+/**
+ * Write register and read back masked value to confirm it is written
+ *
+ * @base - base virtual address.
+ * @offset - register offset.
+ * @mask - register bitmask specifying what should be updated
+ * @val - value to write.
+ */
+static inline void qscratch_write_readback(void __iomem *base, u32 offset,
+		const u32 mask, u32 val)
+{
+	u32 write_val, tmp = readl(base + offset);
+
+	tmp &= ~mask;       /* retain other bits */
+	write_val = tmp | val;
+
+	writel(write_val, base + offset);
+
+	/* Read back to see if val was written */
+	tmp = readl(base + offset);
+	tmp &= mask;        /* clear other bits */
+
+	if (tmp != val)
+		pr_err("write: %x to QSCRATCH: %x FAILED\n", val, offset);
+}
+
+static void dwc_qca961x_enable_vbus_valid(struct dwc3_qca961x *mdwc)
+{
+	if (!mdwc->host) {
+		/* Enable VBUS valid for HS PHY*/
+		qscratch_write_readback(mdwc->qscratch_base, USB30_HS_PHY_CTRL,
+				SW_SESSVLD, SW_SESSVLD);
+		qscratch_write_readback(mdwc->qscratch_base, USB30_HS_PHY_CTRL,
+				UTMI_OTG_VBUS_VALID, UTMI_OTG_VBUS_VALID);
+
+		/* Enable VBUS valid for SS PHY*/
+		qscratch_write_readback(mdwc->qscratch_base, USB30_SS_PHY_CTRL,
+				LANE0_PWR_PRESENT, LANE0_PWR_PRESENT);
+	}
+}
 
 static int dwc3_qca961x_probe(struct platform_device *pdev)
 {
 	struct device_node *node = pdev->dev.of_node;
 	struct dwc3_qca961x *mdwc;
 	int ret = 0;
+	struct resource *res;
+	struct device_node *np = NULL;
 
 	mdwc = devm_kzalloc(&pdev->dev, sizeof(*mdwc), GFP_KERNEL);
-	if (!mdwc)
+	if (!mdwc) {
+		dev_err(mdwc->dev, "failed to get memory\n");
 		return -ENOMEM;
+	}
 
 	platform_set_drvdata(pdev, mdwc);
 
 	mdwc->dev = &pdev->dev;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	mdwc->qscratch_base = devm_ioremap_resource(mdwc->dev, res);
+	if (IS_ERR(mdwc->qscratch_base)) {
+		dev_err(mdwc->dev, "failed to get qscratch res\n");
+		return PTR_ERR(mdwc->qscratch_base);
+	}
 
 	mdwc->master_clk = devm_clk_get(mdwc->dev, "master");
 	if (IS_ERR(mdwc->master_clk)) {
@@ -74,6 +148,14 @@ static int dwc3_qca961x_probe(struct platform_device *pdev)
 		return PTR_ERR(mdwc->srif_25m_clk);
 	}
 
+	np = of_node_get(pdev->dev.of_node);
+	if (of_property_read_u32(np, "qca,host", &mdwc->host)) {
+		dev_err(mdwc->dev,"%s: error reading critical device node properties\n", np->name);
+		return -EFAULT;
+	}
+
+	dwc_qca961x_enable_vbus_valid(mdwc);
+
 	clk_prepare_enable(mdwc->master_clk);
 	clk_prepare_enable(mdwc->mock_utmi_clk);
 	clk_prepare_enable(mdwc->sleep_clk);
@@ -82,7 +164,7 @@ static int dwc3_qca961x_probe(struct platform_device *pdev)
 
 	ret = of_platform_populate(node, NULL, NULL, mdwc->dev);
 	if (ret) {
-		dev_dbg(mdwc->dev, "failed to add create dwc3 core\n");
+		dev_err(mdwc->dev, "failed to add create dwc3 core\n");
 		goto dis_clks;
 	}
 
