27a22e74cc2848535dc7a7257541a88f9ad8457d
Gargi M <mgargi@codeaurora.org>
2015-04-08 16:06:03 +0530

    misc: To add API to authenticate signed binaries using RSA library
    For Wifi firmware authentication, kernel API is required to
    authenticate signed firmware. This API provides interface to
    authenticate RSA-PKCS#v1.5 signed binary, supporting SHA-256, SHA-1 hash
    algorithms.
    
    Change-Id: Ib82bbf3706d94b7723d5a05911406ba45fd9dd93
    Signed-off-by: Gargi M <mgargi@codeaurora.org>

---
+++ include/misc/fw_auth.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <crypto/public_key.h>
+
+enum hash_algo {
+	HASH_ALGO_MD4,
+	HASH_ALGO_MD5,
+	HASH_ALGO_SHA1,
+	HASH_ALGO_RIPE_MD_160,
+	HASH_ALGO_SHA256,
+	HASH_ALGO_SHA384,
+	HASH_ALGO_SHA512,
+	HASH_ALGO_SHA224,
+	HASH_ALGO__LAST
+};
+
+const char *const hash_algo_name[HASH_ALGO__LAST] = {
+	[HASH_ALGO_MD5]     = "md5",
+	[HASH_ALGO_SHA1]    = "sha1",
+	[HASH_ALGO_SHA256]  = "sha256",
+	[HASH_ALGO_SHA384]  = "sha384",
+	[HASH_ALGO_SHA512]  = "sha512",
+	[HASH_ALGO_SHA224]  = "sha224",
+};
+
+struct auth_input {
+	uint8_t *certBuffer;
+	uint8_t *signature;
+	uint8_t *data;
+	int cert_len;
+	int sig_len;
+	int data_len;
+	enum hash_algo sig_hash_algo; /* Hash Algorithm used for signature */
+	enum pkey_algo pk_algo; /* Public Key Algorithm */;
+	enum pkey_id_type cert_type; /* Type of Public Key Certificate */
+};
+
+int authenticate_fw(struct auth_input *ai);
