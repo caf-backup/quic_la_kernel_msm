53f62171cdbd52215ae9ccebc5f82a19ae565f36
ShivaPriya N <shiva_priya_n@codeaurora.org>
2015-04-17 14:21:23 +0530

    sound: ipq40xx: Added PCM driver
    Added PCM RAW driver for VOIP.
    Added the necessary support in the ADSS file and DTSI file.
    Updated the loopback test file for this driver.
    Added Documentation for the device tree entry of loopback test module.
    
    Change-Id: I83769afef83c34e80913bdd064bc4db3fe1dc0a4
    Signed-off-by: ShivaPriya N <shiva_priya_n@codeaurora.org>

--- sound/soc/qca/ipq40xx-pcm-raw-lb-test.c
+++ sound/soc/qca/ipq40xx-pcm-raw-lb-test.c
@@ -24,7 +24,7 @@
 #include <linux/kthread.h>
 #include <linux/string.h>
 #include <linux/platform_device.h>
-#include "ipq-pcm.h"
+#include "ipq40xx-pcm-raw.h"
 
 /*
  * This is an external loopback test module for PCM interface.
@@ -39,35 +39,38 @@
  */
 
 static void pcm_start_test(void);
+static void pcm_fill_data(int32_t *tx_buff, uint32_t size);
 
 /* the test configurations supported */
 #define PCM_LBTEST_8BIT_8KHZ_2CH_TX_TO_RX	1
 #define PCM_LBTEST_8BIT_8KHZ_2CH_RX_TO_TX	101
-#define PCM_LBTEST_16BIT_16KHZ_2CH_TX_TO_RX	2
-#define PCM_LBTEST_16BIT_16KHZ_2CH_RX_TO_TX	201
+#define PCM_LBTEST_16BIT_8KHZ_2CH_TX_TO_RX	2
+#define PCM_LBTEST_16BIT_8KHZ_2CH_RX_TO_TX	201
 #define PCM_LBTEST_16BIT_8KHZ_4CH_TX_TO_RX	3
 #define PCM_LBTEST_16BIT_8KHZ_4CH_RX_TO_TX	301
 
 #define IS_PCM_LBTEST_RX_TO_TX(config)					\
 		((config == PCM_LBTEST_8BIT_8KHZ_2CH_RX_TO_TX) ||	\
-		(config == PCM_LBTEST_16BIT_16KHZ_2CH_RX_TO_TX) ||	\
+		(config == PCM_LBTEST_16BIT_8KHZ_2CH_RX_TO_TX) ||	\
 		(config == PCM_LBTEST_16BIT_8KHZ_4CH_RX_TO_TX))
 
-#define LOOPBACK_SKIP_COUNT		10
+#define LOOPBACK_SKIP_COUNT		30
 #define LOOPBACK_FAIL_THRESHOLD		20
+#define PCM_LB_EMULATION_TEST		1
+
 struct pcm_lb_test_ctx {
 	uint32_t failed;
 	uint32_t passed;
 	unsigned char *last_rx_buff;
 	int running;
 	unsigned char tx_data;
-	unsigned char expected_rx_seq;
 	int read_count;
 	struct task_struct *task;
 };
 
 static struct pcm_lb_test_ctx ctx;
 static unsigned int start;
+struct ipq_pcm_params cfg_params;
 
 static ssize_t show_pcm_lb_value(struct device_driver *driver,
 						char *buff)
@@ -103,10 +106,10 @@ uint32_t pcm_read_write(void)
 {
 	char *rx_buff;
 	char *tx_buff;
-	uint32_t i;
 	uint32_t size;
 
 	size = ipq_pcm_data(&rx_buff, &tx_buff);
+
 	ctx.last_rx_buff = rx_buff;
 
 	if (IS_PCM_LBTEST_RX_TO_TX(start)) {
@@ -114,12 +117,9 @@ uint32_t pcm_read_write(void)
 		memcpy(tx_buff, rx_buff, size);
 	} else {
 		/* get current Tx buffer and write the pattern
-		 * We will write 1, 2, 3, ..., 255, 1, 2, 3...
-		 */
-		for (i =  0; i < size; i++) {
-			tx_buff[i] = ctx.tx_data;
-			ctx.tx_data++;
-		}
+		* We will write 1, 2, 3, ..., 255, 1, 2, 3...
+		*/
+		pcm_fill_data((uint32_t *)tx_buff, size);
 	}
 	ipq_pcm_done();
 	return size;
@@ -127,26 +127,37 @@ uint32_t pcm_read_write(void)
 
 uint32_t pcm_init(void)
 {
-	struct ipq_pcm_params  cfg_params;
 	uint32_t ret = 0;
 
 	switch (start) {
 	case PCM_LBTEST_8BIT_8KHZ_2CH_TX_TO_RX:
 	case PCM_LBTEST_8BIT_8KHZ_2CH_RX_TO_TX:
-		ipq_pcm_init();
+		cfg_params.bit_width = 8;
+		cfg_params.rate = 8000;
+		cfg_params.slot_count = 32;
+		cfg_params.active_slot_count = 4;
+		cfg_params.tx_slots[0] = 0;
+		cfg_params.tx_slots[1] = 1;
+		cfg_params.rx_slots[0] = 0;
+		cfg_params.rx_slots[1] = 1;
+		cfg_params.tx_slots[2] = 2;
+		cfg_params.tx_slots[3] = 3;
+		cfg_params.rx_slots[2] = 2;
+		cfg_params.rx_slots[3] = 3;
+		ret = ipq_pcm_init(&cfg_params);
 		break;
 
-	case PCM_LBTEST_16BIT_16KHZ_2CH_TX_TO_RX:
-	case PCM_LBTEST_16BIT_16KHZ_2CH_RX_TO_TX:
+	case PCM_LBTEST_16BIT_8KHZ_2CH_TX_TO_RX:
+	case PCM_LBTEST_16BIT_8KHZ_2CH_RX_TO_TX:
 		cfg_params.bit_width = 16;
-		cfg_params.rate = 16000;
-		cfg_params.slot_count = 8;
+		cfg_params.rate = 8000;
+		cfg_params.slot_count = 16;
 		cfg_params.active_slot_count = 2;
-		cfg_params.tx_slots[0] = 1;
-		cfg_params.tx_slots[1] = 4;
-		cfg_params.rx_slots[0] = 1;
-		cfg_params.rx_slots[1] = 4;
-		ret = ipq_pcm_init_v2(&cfg_params);
+		cfg_params.tx_slots[0] = 0;
+		cfg_params.tx_slots[1] = 3;
+		cfg_params.rx_slots[0] = 0;
+		cfg_params.rx_slots[1] = 3;
+		ret = ipq_pcm_init(&cfg_params);
 		break;
 
 	case PCM_LBTEST_16BIT_8KHZ_4CH_TX_TO_RX:
@@ -163,7 +174,7 @@ uint32_t pcm_init(void)
 		cfg_params.rx_slots[1] = 1;
 		cfg_params.rx_slots[2] = 8;
 		cfg_params.rx_slots[3] = 9;
-		ret = ipq_pcm_init_v2(&cfg_params);
+		ret = ipq_pcm_init(&cfg_params);
 		break;
 
 	default:
@@ -185,7 +196,15 @@ void process_read(uint32_t size)
 {
 	uint32_t index;
 	static uint32_t continuous_failures;
-	char data;
+	uint32_t *data_u32;
+	uint16_t *data_u16;
+	char val;
+	uint32_t expected_val_u32, rec_val_u32;
+	uint16_t expected_val_u16, rec_val_u16;
+
+	/* get out if test stopped */
+	if (start == 0)
+		return;
 
 	ctx.read_count++;
 	if (ctx.read_count  <= LOOPBACK_SKIP_COUNT) {
@@ -196,27 +215,45 @@ void process_read(uint32_t size)
 		 * Note: our 1st loopback Tx is only after an RX is called.
 		 */
 		return;
-	} else if (ctx.read_count == (LOOPBACK_SKIP_COUNT + 1)) {
-		/*
-		 * our loopback should have settled, so start looking for the
-		 * sequence from here.
-		 */
-		ctx.expected_rx_seq = ctx.last_rx_buff[0];
 	}
-
-	/* get out if test stopped */
-	if (start == 0)
-		return;
-
-	data = ctx.expected_rx_seq;
-	for (index = 0; index < size; index++) {
-		if (ctx.last_rx_buff[index] != (data)) {
-			pr_err("Rx (%d) Failed at index %d: Expected : %d, Received : %d\n",
+	/*
+	 * our loopback should have settled, so start looking for the
+	 * sequence from here. we check only for the data and not for slot
+	 */
+	if (cfg_params.bit_width == 16) {
+		data_u32 = (uint32_t *)ctx.last_rx_buff;
+		val = data_u32[0] & 0xFF;
+		size = size / 4; /* as we are checking data as uint32 */
+		for (index = 0; index < size; index++) {
+			expected_val_u32 = val;
+			rec_val_u32 = data_u32[index] & (0xFFFF);
+			if (expected_val_u32 != rec_val_u32) {
+				pr_err("\nRx(%d) Failed at index %d: Expected : 0x%x, Received : 0x%x\n ",
 					ctx.read_count, index,
-					data, ctx.last_rx_buff[index]);
-			break;
+					expected_val_u32, rec_val_u32);
+				break;
+			}
+			val++;
 		}
-		data++;
+	} else {
+		data_u16 = (uint16_t *)ctx.last_rx_buff;
+		val = data_u16[0] & 0xFF;
+		size = size / 2; /* as we are checking as uint16 */
+		for (index = 0; index < size; index++) {
+			rec_val_u16 = data_u16[index];
+			/* check for validity flag */
+			if (rec_val_u16 & 0x8000) {
+				expected_val_u16 = val;
+				rec_val_u16 &= 0xFF;
+				val++;
+				if (expected_val_u16 != rec_val_u16) {
+					pr_err(KERN_INFO "\nRx(%d) Failed at index %d: Expected : 0x%x, Received : 0x%x\n ",
+						ctx.read_count, index,
+						expected_val_u16, rec_val_u16);
+					break;
+				}
+			}
+		}
 	}
 
 	if (index == size) {
@@ -227,20 +264,41 @@ void process_read(uint32_t size)
 		continuous_failures++;
 	}
 
-	ctx.expected_rx_seq += size;
-
 	/* Abort if there are more failures */
 	if (continuous_failures >= LOOPBACK_FAIL_THRESHOLD) {
-		pr_err("Aborting loopback test as there are %d continuous failures\n",
-					continuous_failures);
+		pr_err("\nAborting loopback test as there are %d continuous failures\n",
+				continuous_failures);
 		continuous_failures = 0;
 		ctx.running = 0; /* stops test thread (current) */
+	}
+}
+static void pcm_fill_data(int32_t *tx_buff, uint32_t size)
+{
+	uint32_t i, slot;
+
+	if (cfg_params.bit_width == 16) {
+		slot = cfg_params.active_slot_count;
+		for (i = 0; i < size / 4; i++) {
+			tx_buff[i] = ctx.tx_data;
+			tx_buff[i] |=
+				cfg_params.tx_slots[(ctx.tx_data % slot)] << 16;
+			ctx.tx_data++;
+		}
+	} else {
+		slot = cfg_params.active_slot_count;
+		for (i = 0; i < size / 4; i++) {
+			tx_buff[i] = ctx.tx_data;
+			tx_buff[i] |=
+				cfg_params.tx_slots[(ctx.tx_data % slot)] << 8;
+			tx_buff[i] |= 1 << 15; /* valid bit */
+			ctx.tx_data++;
+		}
 	}
 }
 
 int pcm_test_rw(void *data)
 {
-	struct sched_param param;
+	/* struct sched_param param; */
 	uint32_t ret;
 	uint32_t size;
 
@@ -248,12 +306,15 @@ int pcm_test_rw(void *data)
 	 * set test thread priority as 90, this is to align with what
 	 * D2 VOIP stack does.
 	 */
+#ifdef PCM_LB_EMULATION_TEST
+	struct sched_param param;
 
 	param.sched_priority = 90;
 	ret = sched_setscheduler(ctx.task, SCHED_FIFO, &param);
 	if (ret)
 		pr_err("%s : Error setting priority, error: %d\n",
-				__func__, ret);
+						__func__, ret);
+#endif
 
 	ret = pcm_init();
 	if (ret) {
@@ -263,13 +324,14 @@ int pcm_test_rw(void *data)
 
 	pr_notice("%s : Test thread started\n", __func__);
 	ctx.running = 1;
+
 	while (ctx.running) {
 		size = pcm_read_write();
 		if (!IS_PCM_LBTEST_RX_TO_TX(start))
 			process_read(size);
 	}
 	pr_notice("%s : Test Thread stopped\n", __func__);
-	/* for rx to tx loopback, we cnanot detect failures */
+	/* for rx to tx loopback, we cannot detect failures */
 	if (!IS_PCM_LBTEST_RX_TO_TX(start))
 		pr_notice("\nPassed : %d, Failed : %d\n",
 					ctx.passed, ctx.failed);
@@ -300,12 +362,13 @@ static void pcm_start_test(void)
 	}
 }
 
-int pcm_test_init(struct platform_device *pdev)
+static int ipq40xx_pcm_lb_probe(struct platform_device *pdev)
 {
+	start = 0;
 	return pcm_lb_drv_attr_init(pdev);
 }
 
-void pcm_test_exit(struct platform_device *pdev)
+static int ipq40xx_pcm_lb_remove(struct platform_device *pdev)
 {
 	pcm_lb_drv_attr_deinit(pdev);
 	if (ctx.running) {
@@ -313,4 +376,27 @@ void pcm_test_exit(struct platform_device *pdev)
 		/* wait sufficient time for test thread to finish */
 		mdelay(2000);
 	}
+	return 0;
 }
+
+#define DRIVER_NAME "ipq40xx_pcm_raw_lb"
+
+static const struct of_device_id qca_raw_lb_match_table[] = {
+	{ .compatible = "qca,ipq40xx-pcm-lb", },
+	{},
+};
+
+static struct platform_driver ipq40xx_pcm_raw_driver_test = {
+	.probe          = ipq40xx_pcm_lb_probe,
+	.remove         = ipq40xx_pcm_lb_remove,
+	.driver = {
+		.name           = DRIVER_NAME,
+		.of_match_table = qca_raw_lb_match_table,
+	},
+};
+
+module_platform_driver(ipq40xx_pcm_raw_driver_test);
+
+MODULE_ALIAS(DRIVER_NAME);
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_DESCRIPTION("QCA RAW PCM VoIP Platform Driver Loopback Test");
