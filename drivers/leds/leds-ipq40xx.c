23341ef0688324a1a800290ed3bb3c051d46cbec
Senthilkumar N L <snlakshm@codeaurora.org>
2015-05-04 00:18:31 +0530

    ipq40xx: led: replace qca961x with ipq40xx
    Change-Id: Ifb06f3eb9be50870577fe513a405f4633a6d2073
    Signed-off-by: Senthilkumar N L <snlakshm@codeaurora.org>

--- drivers/leds/leds-ipq40xx.c
+++ drivers/leds/leds-ipq40xx.c
@@ -25,7 +25,7 @@
 #define LEDC_REG_SIZE	4
 #define BUF_NAME_LEN	25
 #define MAX_BLINK_IDX	4
-#define LEDC_DRV_NAME	"qca961x-leds"
+#define LEDC_DRV_NAME	"ipq40xx-leds"
 #define LEDC_ADDR(x)	(ledc_base_addr + LEDC_BASE_REG_OFFSET + \
 				(LEDC_REG_SIZE * (x)))
 
@@ -62,22 +62,22 @@ enum blink_duty_values {
 	BLINK_DUTY_75
 };
 
-struct qca961x_led_data {
+struct ipq40xx_led_data {
 	struct led_classdev cdev;
 	int led_blink_idx;
 };
 
-static struct qca961x_led_data *leds;
+static struct ipq40xx_led_data *leds;
 static void *ledc_base_addr;
 static int ledc_base_reg_offset;
 static int blink_idx_cnt;
 static int led_blink_array[MAX_BLINK_IDX];
 
-static int qca961x_set_led_blink_set(struct led_classdev *led_cdev,
+static int ipq40xx_set_led_blink_set(struct led_classdev *led_cdev,
 		unsigned long *delay_on, unsigned long *delay_off)
 {
-	struct qca961x_led_data *led_dat = container_of(led_cdev,
-					struct qca961x_led_data, cdev);
+	struct ipq40xx_led_data *led_dat = container_of(led_cdev,
+					struct ipq40xx_led_data, cdev);
 	int blink_enable, blink_freq, blink_duty, blink_value,
 					duty_cycle, total_on_off;
 	int reg, mask;
@@ -131,11 +131,11 @@ static int qca961x_set_led_blink_set(struct led_classdev *led_cdev,
 	return 0;
 }
 
-static void qca961x_set_led_brightness_set(struct led_classdev *led_cdev,
+static void ipq40xx_set_led_brightness_set(struct led_classdev *led_cdev,
 		enum led_brightness brightness)
 {
-	struct qca961x_led_data *led_dat = container_of(led_cdev,
-					struct qca961x_led_data, cdev);
+	struct ipq40xx_led_data *led_dat = container_of(led_cdev,
+					struct ipq40xx_led_data, cdev);
 	int reg, mask;
 	unsigned long delay_on, delay_off;
 	delay_on = 0;
@@ -147,11 +147,11 @@ static void qca961x_set_led_brightness_set(struct led_classdev *led_cdev,
 	else
 		reg = reg | mask;
 	/* Clear blink settings. Setting brightness overrides blink settings. */
-	qca961x_set_led_blink_set(led_cdev, &delay_on, &delay_off);
+	ipq40xx_set_led_blink_set(led_cdev, &delay_on, &delay_off);
 	writel(reg, LEDC_ADDR(LEDC_CG6_OFFSET));
 }
 
-static int __init qca961x_led_probe(struct platform_device *pdev)
+static int __init ipq40xx_led_probe(struct platform_device *pdev)
 {
 	int ret, i;
 	uint32_t val_arr[LEDC_MAX_OFFSET];
@@ -212,11 +212,11 @@ static int __init qca961x_led_probe(struct platform_device *pdev)
 		leds[i].led_blink_idx = i;
 		leds[i].cdev.brightness = LED_OFF;
 		leds[i].cdev.max_brightness = 1;
-		leds[i].cdev.brightness_set = qca961x_set_led_brightness_set;
-		leds[i].cdev.blink_set = qca961x_set_led_blink_set;
+		leds[i].cdev.brightness_set = ipq40xx_set_led_brightness_set;
+		leds[i].cdev.blink_set = ipq40xx_set_led_blink_set;
 
 		memset(buf, 0, BUF_NAME_LEN);
-		snprintf(buf, BUF_NAME_LEN, "qca961x::led%d", i);
+		snprintf(buf, BUF_NAME_LEN, "ipq40xx::led%d", i);
 		leds[i].cdev.name = buf;
 
 		ret = led_classdev_register(&pdev->dev, &leds[i].cdev);
@@ -239,7 +239,7 @@ err_iounmap:
 	return ret;
 }
 
-static int __exit qca961x_led_remove(struct platform_device *pdev)
+static int __exit ipq40xx_led_remove(struct platform_device *pdev)
 {
 	int i;
 	if (leds) {
@@ -256,22 +256,22 @@ static int __exit qca961x_led_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct of_device_id qca961x_match_table[] = {
+static struct of_device_id ipq40xx_match_table[] = {
 	{.compatible = "qca,ledc"},
 	{},
 };
 
-static struct platform_driver qca961x_led_driver = {
-	.probe  = qca961x_led_probe,
-	.remove = qca961x_led_remove,
+static struct platform_driver ipq40xx_led_driver = {
+	.probe  = ipq40xx_led_probe,
+	.remove = ipq40xx_led_remove,
 	.driver = {
 		.name   = LEDC_DRV_NAME,
 		.owner  = THIS_MODULE,
-		.of_match_table = qca961x_match_table,
+		.of_match_table = ipq40xx_match_table,
 	},
 };
 
-module_platform_driver(qca961x_led_driver);
+module_platform_driver(ipq40xx_led_driver);
 
 MODULE_LICENSE("GPL v2");
 MODULE_VERSION("0.1");
