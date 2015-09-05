40f074980dffabb66ad01024b678140b8989bccd
Bharath Gandhi <bgandhi@codeaurora.org>
2015-09-05 21:55:09 +0530

    ipq806x: Enabled Red LED (GPIO-9) during init for AK01_1XX board
    Enable RED LED through GPIO-9 for the AK01_1XX board type.
    This change muxes GPIO 9 which controlled the IP-Dock LED.
    This change is specific to AK01_1XX board type.
    
    Change-Id: I4b83c7aee445e447b5d99127e151ebde60eb89ee
    Signed-off-by: Bharath Gandhi <bgandhi@codeaurora.org>

--- arch/arm/mach-msm/board-ipq806x.c
+++ arch/arm/mach-msm/board-ipq806x.c
@@ -2072,6 +2072,7 @@ static struct gpio_led ak01_gpio_leds[] = {
 		.name       = "ak01:red:fp",
 		.gpio       = 9,
 		.active_low = 0,
+		.default_state = LEDS_GPIO_DEFSTATE_ON,
 	},
 	{
 		.name       = "ak01:green:status",
