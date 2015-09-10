b7c90e1cb9537f1c30e1764fa8b98f8464552749
Kirthik Srinivasan <kirthik@codeaurora.org>
2015-09-10 16:59:52 +0530

    pinctrl: Populate default values pinctrl structure
    Fill values for extra funtion and extra bits for all platforms.
    
    Change-Id: I58947053efec7b3da9f99bea54ada7231769770b
    Signed-off-by: Kirthik Srinivasan <kirthik@codeaurora.org>

--- drivers/pinctrl/qcom/pinctrl-ipq8064.c
+++ drivers/pinctrl/qcom/pinctrl-ipq8064.c
@@ -216,6 +216,9 @@ static const unsigned int sdc3_data_pins[] = { 71 };
 		.intr_polarity_bit = 1,			\
 		.intr_detection_bit = 2,		\
 		.intr_detection_width = 1,		\
+		.extra_val = 0,				\
+		.extra_mask = 0,			\
+		.extra_func = IPQ_MUX_NA,		\
 	}
 
 #define SDC_PINGROUP(pg_name, ctl, pull, drv)		\
@@ -242,6 +245,9 @@ static const unsigned int sdc3_data_pins[] = { 71 };
 		.intr_polarity_bit = -1,                \
 		.intr_detection_bit = -1,               \
 		.intr_detection_width = -1,             \
+		.extra_val = 0,				\
+		.extra_mask = 0,			\
+		.extra_func = IPQ_MUX_NA,		\
 	}
 
 enum ipq8064_functions {
