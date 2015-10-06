2a2fb9a63903a9eb5f40b2d7519fb2fbe6244be2
Natesh babu <nrameg@codeaurora.org>
2015-10-06 11:24:41 +0530

    ipq40xx: ledc: Fix compiler warnings
    Change-Id: Ice3e252e532b1ad48fa467bada3ef53d3d2edc2a
    Signed-off-by: Natesh babu <nrameg@codeaurora.org>

--- drivers/leds/leds-ipq40xx.c
+++ drivers/leds/leds-ipq40xx.c
@@ -70,7 +70,6 @@ struct ipq40xx_led_data {
 
 static struct ipq40xx_led_data *leds;
 static void *ledc_base_addr;
-static int ledc_base_reg_offset;
 static int blink_idx_cnt;
 static int led_blink_array[MAX_BLINK_IDX];
 
@@ -197,7 +196,8 @@ static int __init ipq40xx_led_probe(struct platform_device *pdev)
 	ret = of_property_read_u32_array(of_node, "qcom,tcsr_ledc_values",
 		val_arr, LEDC_MAX_OFFSET);
 	if (ret) {
-		dev_err(&pdev->dev, "invalid or missing property: qcom,tcsr_ledc_values\n");
+		dev_err(&pdev->dev,
+		"invalid or missing property: qcom,tcsr_ledc_values\n");
 		return -ENODEV;
 	}
 
@@ -216,7 +216,8 @@ static int __init ipq40xx_led_probe(struct platform_device *pdev)
 	ret = of_property_read_u32_array(of_node, "qcom,ledc_blink_indices",
 		led_blink_array, blink_idx_cnt);
 	if (ret) {
-		dev_err(&pdev->dev, "invalid or missing property: blink indices\n");
+		dev_err(&pdev->dev,
+			"invalid or missing property: blink indices\n");
 		return -ENODEV;
 	}
 
