83bdcab2f8f456d422f2bb97d759f750ee6857c8
Mohammed Shafi Shajakhan <mohammed@qti.qualcomm.com>
2016-02-12 15:24:58 +0530

    CHROMIUM: ath10k: Ignore survey update during on going scan
    Ignore survey update during on going scan, this change is
    directly picked from 3.18 wireless stack
    
    BUG=chrome-os-partner:51964
    TEST=tested by running suvey dumps using iw.
    
    (cherry picked from commit 4348de2781f283bbe72cceafd85de36e979deea0)
    (source: wireless/kernel v3.14)
    
    Change-Id: I13204ca06667834839c4ddd888d812dc3e40ff4a
    Signed-off-by:  Mohammed Shafi Shajakhan <mohammed@qti.qualcomm.com>
    Reviewed-on: https://chromium-review.googlesource.com/340732
    Commit-Ready: Yixiang Li <yixiang@google.com>
    Tested-by: Yixiang Li <yixiang@google.com>
    Reviewed-by: Srinivasa duvvuri <sduvvuri@chromium.org>
    Signed-off-by: Vikas Patel <vikpatel@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -5071,6 +5071,12 @@ static int ath10k_wmi_event_pdev_bss_chan_info(struct ath10k *ar,
 		   freq, noise_floor, busy, total, tx, rx, rx_bss);
 
 	spin_lock_bh(&ar->data_lock);
+
+	if (ar->scan.state != ATH10K_SCAN_IDLE) {
+		ath10k_warn(ar, "received chan survey update when scan ongoing ignoring\n");
+		goto exit;
+	}
+
 	idx = freq_to_idx(ar, freq);
 	if (idx >= ARRAY_SIZE(ar->survey)) {
 		ath10k_warn(ar, "bss chan info: invalid frequency %d (idx %d out of bounds)\n",
