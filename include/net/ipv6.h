fae4b6d7add2b8e4c99396f0d3c4d7fc59078dcf
Zhi Chen <zhichen@codeaurora.org>
2016-02-17 11:14:35 -0800

    Revert "ipv6: fix NULL dereference in udp6_ufo_fragment()"
    This reverts commit c92e31bf118e5f7f461ab5b7cf9c87489ffb93a3.
    
    Change-Id: I47d0433b2344ad0dd7c6fb1fa9b992f804cd2a47
    Signed-off-by: Zhi Chen <zhichen@codeaurora.org>

--- include/net/ipv6.h
+++ include/net/ipv6.h
@@ -484,8 +484,7 @@ static inline int ipv6_addr_diff(const struct in6_addr *a1, const struct in6_add
 	return __ipv6_addr_diff(a1, a2, sizeof(struct in6_addr));
 }
 
-extern void ipv6_select_ident(struct frag_hdr *fhdr, struct in6_addr *daddr,
-			      struct in6_addr *saddr);
+extern void ipv6_select_ident(struct frag_hdr *fhdr, struct rt6_info *rt);
 
 /*
  *	Prototypes exported by ipv6
--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -598,8 +598,7 @@ int ip6_find_1stfragopt(struct sk_buff *skb, u8 **nexthdr)
 	return offset;
 }
 
-void ipv6_select_ident(struct frag_hdr *fhdr, struct in6_addr *daddr,
-		       struct in6_addr *saddr)
+void ipv6_select_ident(struct frag_hdr *fhdr, struct rt6_info *rt)
 {
 	static u32 ip6_idents_hashrnd __read_mostly;
 	static bool hashrnd_initialized = false;
@@ -609,8 +608,8 @@ void ipv6_select_ident(struct frag_hdr *fhdr, struct in6_addr *daddr,
 		hashrnd_initialized = true;
 		get_random_bytes(&ip6_idents_hashrnd, sizeof(ip6_idents_hashrnd));
 	}
-	hash = __ipv6_addr_jhash(daddr, ip6_idents_hashrnd);
-	hash = __ipv6_addr_jhash(saddr, hash);
+	hash = __ipv6_addr_jhash(&rt->rt6i_dst.addr, ip6_idents_hashrnd);
+	hash = __ipv6_addr_jhash(&rt->rt6i_src.addr, hash);
 
 	id = ip_idents_reserve(hash, 1);
 	fhdr->identification = htonl(id);
@@ -701,7 +700,7 @@ int ip6_fragment(struct sk_buff *skb, int (*output)(struct sk_buff *))
 		skb_reset_network_header(skb);
 		memcpy(skb_network_header(skb), tmp_hdr, hlen);
 
-		ipv6_select_ident(fh, &rt->rt6i_dst.addr, &rt->rt6i_src.addr);
+		ipv6_select_ident(fh, rt);
 		fh->nexthdr = nexthdr;
 		fh->reserved = 0;
 		fh->frag_off = htons(IP6_MF);
@@ -850,8 +849,7 @@ slow_path:
 		fh->nexthdr = nexthdr;
 		fh->reserved = 0;
 		if (!frag_id) {
-			ipv6_select_ident(fh, &rt->rt6i_dst.addr,
-					  &rt->rt6i_dst.addr);
+			ipv6_select_ident(fh, rt);
 			frag_id = fh->identification;
 		} else
 			fh->identification = frag_id;
@@ -1152,8 +1150,7 @@ static inline int ip6_ufo_append_data(struct sock *sk,
 		skb_shinfo(skb)->gso_size = (mtu - fragheaderlen -
 					     sizeof(struct frag_hdr)) & ~7;
 		skb_shinfo(skb)->gso_type = SKB_GSO_UDP;
-		ipv6_select_ident(&fhdr, &rt->rt6i_dst.addr,
-				  &rt->rt6i_dst.addr);
+		ipv6_select_ident(&fhdr, rt);
 		skb_shinfo(skb)->ip6_frag_id = fhdr.identification;
 		__skb_queue_tail(&sk->sk_write_queue, skb);
 	}
