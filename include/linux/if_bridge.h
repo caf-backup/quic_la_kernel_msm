cdb4dda5d1be1dc128c896d2ab4adb2c50198f20
Murat Sezgin <msezgin@codeaurora.org>
2016-03-08 21:49:10 -0800

    Pass serial number to Linux bridge port lookup
    - This allows the same ECM lookup function to be used in interface
      hierarchy generation and when updating stats (so no difference in
      lookup even if the priority field changes)
    - If the priority field changes, will update the H-Active entry and
      generate a debug log
    
    Change-Id: Ic15a8e87d50dead780b5702ccb363b1a0c487463
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>

--- include/linux/if_bridge.h
+++ include/linux/if_bridge.h
@@ -103,7 +103,7 @@ struct __fdb_entry {
 
 extern void brioctl_set(int (*ioctl_hook)(struct net *, unsigned int, void __user *));
 extern struct net_device *br_port_dev_get(struct net_device *dev, unsigned char *addr,
-		struct sk_buff *skb);
+		struct sk_buff *skb, unsigned int cookie);
 extern void br_refresh_fdb_entry(struct net_device *dev, const char *addr);
 extern void br_dev_update_stats(struct net_device *dev, struct rtnl_link_stats64 *nlstats);
 extern bool br_fdb_has_entry(struct net_device *dev, const char *addr);
@@ -118,7 +118,7 @@ typedef struct net_bridge_port *br_get_dst_hook_t(const struct net_bridge_port *
 extern br_get_dst_hook_t __rcu *br_get_dst_hook;
 
 typedef struct net_bridge_port *br_port_dev_get_hook_t(struct net_device *dev,
-		struct sk_buff *skb, unsigned char *addr);
+		struct sk_buff *skb, unsigned char *addr, unsigned int cookie);
 extern br_port_dev_get_hook_t __rcu *br_port_dev_get_hook;
 
 typedef int (br_multicast_handle_hook_t)(const struct net_bridge_port *src,
