cc069eefab811598d49b75c41e17ce3ca9121215
Shyam Sunder <ssunde@codeaurora.org>
2016-12-20 20:21:43 +0530

    net: l2tp: ppp_channel_proto_version API
    New API added to retrieve channel version info
    
    Change-Id: I3222724b7f0939c0b087b2a2904dd4fd66ee1fce
    Signed-off-by: Shyam Sunder <ssunde@codeaurora.org>

--- include/uapi/linux/if.h
+++ include/uapi/linux/if.h
@@ -86,6 +86,9 @@
 #define IFF_MACVLAN 0x200000		/* Macvlan device */
 #define IFF_NO_IP_ALIGN	0x200000	/* do not ip-align allocated rx pkts */
 #define IFF_TUN_TAP 0x10000000		/* virtual tunnel device */
+#define IFF_PPP_L2TPV2 0x1000000
+#define IFF_PPP_L2TPV3 0x2000000
+#define IFF_PPP_PPTP 0x4000000
 
 
 #define IF_GET_IFACE	0x0001		/* for querying only */
@@ -133,7 +136,7 @@ enum {
 };
 
 /*
- *	Device mapping structure. I'd just gone off and designed a 
+ *	Device mapping structure. I'd just gone off and designed a
  *	beautiful scheme using only loadable modules with arguments
  *	for driver options and along come the PCMCIA people 8)
  *
@@ -145,7 +148,7 @@ enum {
 struct ifmap {
 	unsigned long mem_start;
 	unsigned long mem_end;
-	unsigned short base_addr; 
+	unsigned short base_addr;
 	unsigned char irq;
 	unsigned char dma;
 	unsigned char port;
@@ -182,7 +185,7 @@ struct ifreq {
 	{
 		char	ifrn_name[IFNAMSIZ];		/* if name, e.g. "en0" */
 	} ifr_ifrn;
-	
+
 	union {
 		struct	sockaddr ifru_addr;
 		struct	sockaddr ifru_dstaddr;
