9ff7d6a4055efb7ebae2e26fad574ccc610a47e2
Gopikrishna Mogasati <gmogas@codeaurora.org>
2017-05-05 16:04:35 +0530

    diag: Add proper synchronization checks to msg mask table
    This fix removes dependency between real time message mask
    table and build time message mask table. Also this fix
    synchronizes retrieval and modification of real time message
    mask table.
    
    CRs-Fixed: 2015227
    Change-Id: Id0a0964337ec4645d7061fc35120dfa061a990ff
    Signed-off-by: Gopikrishna Mogasati <gmogas@codeaurora.org>
    Signed-off-by: Miaoqing Pan <miaoqing@codeaurora.org>

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -3300,6 +3300,10 @@ static int diag_probe(struct platform_device *pdev)
 	mutex_init(&driver->diag_file_mutex);
 	mutex_init(&driver->delayed_rsp_mutex);
 	mutex_init(&apps_data_mutex);
+	mutex_init(&driver->msg_mask_lock);
+	for (i = 0; i < NUM_PERIPHERALS; i++)
+		mutex_init(&driver->diagfwd_channel_mutex[i]);
+	mutex_init(&driver->diagfwd_untag_mutex);
 	init_waitqueue_head(&driver->wait_q);
 	INIT_WORK(&(driver->diag_drain_work), diag_drain_work_fn);
 	INIT_WORK(&(driver->update_user_clients),
