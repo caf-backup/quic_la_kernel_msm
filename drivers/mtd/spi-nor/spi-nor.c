267cf30e4972220f200a9bd3afb41a86f7c99d4e
Miaoqing Pan <miaoqing@codeaurora.org>
2017-06-08 13:35:13 +0800

    ar71xx: patch-745: add Win 512k NOR OTP flash driver support
    Change-Id: I92f8dd8d552141405a8d5def2ffac0f0cd95b802
    Signed-off-by: Zhi Chen <zhichen@codeaurora.org>
    Signed-off-by: Miaoqing Pan <miaoqing@codeaurora.org>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -870,6 +870,7 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "w25q128fw", INFO(0xef6018, 0, 64 * 1024, 256, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "w25q80", INFO(0xef5014, 0, 64 * 1024,  16, SECT_4K) },
 	{ "w25q80bl", INFO(0xef4014, 0, 64 * 1024,  16, SECT_4K) },
+	{ "w25q40cl", INFO(0xef4013, 0, 64 * 1024, 8, SECT_4K) },
 	{ "w25q128", INFO(0xef4018, 0, 64 * 1024, 256, SECT_4K) },
 	{ "w25q256", INFO(0xef4019, 0, 64 * 1024, 512, SECT_4K) },
 
