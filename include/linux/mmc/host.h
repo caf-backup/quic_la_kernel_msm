eee806bf3b4bd766a4b06655f225cf4a13f6f482
Md Sadre Alam <mdalam@codeaurora.org>
2018-03-06 14:22:40 +0530

    Revert "ipq807x: sdhci-msm: Remove Inline Crypto Engine Support"
    This reverts commit e9b2d8dcbd96e601f25bf89aef8cb12f36599b7b.
    
    Change-Id: I8ad74e2ccd8a6cce985f731daf70c89a4084196b
    Signed-off-by: Md Sadre Alam <mdalam@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -290,7 +290,7 @@ struct mmc_host {
 #define MMC_CAP2_SDIO_IRQ_NOTHREAD (1 << 17)
 #define MMC_CAP2_NO_WRITE_PROTECT (1 << 18)	/* No physical write protect pin, assume that card is always read-write */
 #define MMC_CAP2_HS400_POST_TUNING (1 << 19)
-
+#define MMC_CAP2_PACKED_WR_CONTROL (1 << 20)	/* Allow write packing control */
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
 	/* host specific block data */
