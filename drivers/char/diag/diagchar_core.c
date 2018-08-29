c8df180927a5f48343f0b6a8f9d1d6bb7f67e6bc
Manoj Prabhu B <bmanoj@codeaurora.org>
2018-08-29 13:07:07 +0530

    diag: Prevent out of bound access while initializing msg mask
    Move the mask_info mutex initialization outside mask structure
    to facilitate prevention of out of bound access while initializing
    msg mask during md session creation. Use separate msg_mask_tbl_count
    for ODL session msg mask and regular msg mask to prevent out of
    bound access in a possible race condition of accessing mask ranges.
    
    The chances of accessing uninitialized mask is prevented
    by adding null pointer checks for the mask structure and its
    member pointer.
    
    Change-Id: I87497c67daff8cc1797a1266d50456bdbd3a9c23
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>
    Signed-off-by: Chetan C R <cravin@codeaurora.org>
    (cherry picked from commit 7d9f0b8bd4528366c90682565d612f6252c2e274)

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -1138,7 +1138,8 @@ static void diag_md_session_exit(void)
 			diag_log_mask_free(session_info->log_mask);
 			kfree(session_info->log_mask);
 			session_info->log_mask = NULL;
-			diag_msg_mask_free(session_info->msg_mask);
+			diag_msg_mask_free(session_info->msg_mask,
+				session_info);
 			kfree(session_info->msg_mask);
 			session_info->msg_mask = NULL;
 			diag_event_mask_free(session_info->event_mask);
@@ -1246,7 +1247,9 @@ int diag_md_session_create(int mode, int peripheral_mask, int proc)
 			 "return value of event copy. err %d\n", err);
 		goto fail_peripheral;
 	}
-	err = diag_msg_mask_copy(new_session->msg_mask, &msg_mask);
+	new_session->msg_mask_tbl_count = 0;
+	err = diag_msg_mask_copy(new_session, new_session->msg_mask,
+		&msg_mask);
 	if (err) {
 		DIAG_LOG(DIAG_DEBUG_USERSPACE,
 			 "return value of msg copy. err %d\n", err);
@@ -1281,7 +1284,8 @@ fail_peripheral:
 	diag_event_mask_free(new_session->event_mask);
 	kfree(new_session->event_mask);
 	new_session->event_mask = NULL;
-	diag_msg_mask_free(new_session->msg_mask);
+	diag_msg_mask_free(new_session->msg_mask,
+		new_session);
 	kfree(new_session->msg_mask);
 	new_session->msg_mask = NULL;
 fail_normal:
@@ -1318,6 +1322,17 @@ static void diag_md_session_close(struct diag_md_session_t *session_info)
 		session_info->event_mask = NULL;
 		del_timer(&session_info->hdlc_reset_timer);
 	}
+	diag_log_mask_free(session_info->log_mask);
+	kfree(session_info->log_mask);
+	session_info->log_mask = NULL;
+	diag_msg_mask_free(session_info->msg_mask,
+			  session_info);
+	kfree(session_info->msg_mask);
+	session_info->msg_mask = NULL;
+	diag_event_mask_free(session_info->event_mask);
+	kfree(session_info->event_mask);
+	session_info->event_mask = NULL;
+	del_timer(&session_info->hdlc_reset_timer);
 
 	for (i = 0; i < NUM_MD_SESSIONS && !found; i++) {
 		if (driver->md_session_map[i] != NULL)
