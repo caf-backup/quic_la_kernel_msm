6f979e115d23f8fcdfb5f4d07e08b5f8dfa01012
AnilKumar Chimata <anilc@codeaurora.org>
2018-09-27 17:31:26 +0530

    INTERNAL: char: hw_random: Modify driver to handle more data.
    Adds retry mechanism for use-cases where more data is needed,
    currently data is exhausted if data is read continuously. Also
    adds support for skipping configuration for platforms if config
    is handled on secure side.
    
    INTERNAL: char: hw_random: Use interconnect API for bw requests
    
    Few use-cases needs CPU and other cores bandwidth to get
    maximum performance, hence using the interconnect API to
    set bw requests for prng.
    
    INTERNAL: ARM: dts: msm: Enable PRNG driver for sdm845
    
    Enable PRNG device node on include file to enable
    the PRNG driver to communicate with /dev/random.
    
    INTERNAL: firmware: qcom: scm: Update qcom_scm_call signature
    
    New syscall is added which has the requirement to update
    existing qcom_scm_call, hence signature is changed.
    
    INTERNAL: crypto: qce: ice: Add support for Inline Crypto Engine
    
    This patch adds support for Inline Crypto Engine (ICE), which
    is embedded into storage device/controller such as UFS/eMMC.
    ICE is intended for high throughput cryptographic encryption
    or decryption of storage data.
    
    INTERNAL: ARM: dts: msm: Add UFS ICE node for sdm845
    
    Add ICE device node with the required configuration on
    the include file, which is needed to initialize the ICE
    driver for sdm845 to perform crypto related operations.
    
    INTERNAL: defconfig: Enable ICE driver for compilation
    
    Enable ICE driver compilation by default for sdm845
    target which is needed for data encryption or decryption
    during storage writes or reads.
    
    Change-Id: I0cacb1a3bc2b0d9ffd9480ae05d2b4620184c517
    Signed-off-by: AnilKumar Chimata <anilc@codeaurora.org>
    Signed-off-by: Mohammed Thasleem <mthasl@codeaurora.org>

--- drivers/char/hw_random/msm-rng.c
+++ drivers/char/hw_random/msm-rng.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2013, 2018 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -18,6 +18,8 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
+#include <linux/delay.h>
+#include <linux/interconnect.h>
 
 /* Device specific register offsets */
 #define PRNG_DATA_OUT		0x0000
@@ -34,11 +36,19 @@
 #define MAX_HW_FIFO_DEPTH	16
 #define MAX_HW_FIFO_SIZE	(MAX_HW_FIFO_DEPTH * 4)
 #define WORD_SZ			4
+#define PRNG_RETRY_COUNT	5
+#define PRNG_RETRY_DELAY_USEC	440
 
 struct msm_rng {
 	void __iomem *base;
 	struct clk *clk;
 	struct hwrng hwrng;
+	uint32_t qrng_perf_client;
+	struct mutex rng_lock;
+	bool skip_qrng_config;
+	struct icc_path *path;
+	uint32_t min_bw;
+	uint32_t max_bw;
 };
 
 #define to_msm_rng(p)	container_of(p, struct msm_rng, hwrng)
@@ -49,6 +59,14 @@ static int msm_rng_enable(struct hwrng *hwrng, int enable)
 	u32 val;
 	int ret;
 
+	/*
+	 * Configuration of these registers happen on secure side
+	 * on latest platforms hence skipping this part.
+	 */
+	if (rng->skip_qrng_config)
+		return 0;
+
+	icc_set(rng->path, 0, rng->max_bw);
 	ret = clk_prepare_enable(rng->clk);
 	if (ret)
 		return ret;
@@ -75,6 +93,7 @@ static int msm_rng_enable(struct hwrng *hwrng, int enable)
 
 already_enabled:
 	clk_disable_unprepare(rng->clk);
+	icc_set(rng->path, 0, rng->min_bw);
 	return 0;
 }
 
@@ -83,6 +102,7 @@ static int msm_rng_read(struct hwrng *hwrng, void *data, size_t max, bool wait)
 	struct msm_rng *rng = to_msm_rng(hwrng);
 	size_t currsize = 0;
 	u32 *retdata = data;
+	uint32_t failed = 0;
 	size_t maxsize;
 	int ret;
 	u32 val;
@@ -90,6 +110,8 @@ static int msm_rng_read(struct hwrng *hwrng, void *data, size_t max, bool wait)
 	/* calculate max size bytes to transfer back to caller */
 	maxsize = min_t(size_t, MAX_HW_FIFO_SIZE, max);
 
+	mutex_lock(&rng->rng_lock);
+	icc_set(rng->path, 0, rng->max_bw);
 	ret = clk_prepare_enable(rng->clk);
 	if (ret)
 		return ret;
@@ -97,22 +119,30 @@ static int msm_rng_read(struct hwrng *hwrng, void *data, size_t max, bool wait)
 	/* read random data from hardware */
 	do {
 		val = readl_relaxed(rng->base + PRNG_STATUS);
-		if (!(val & PRNG_STATUS_DATA_AVAIL))
-			break;
+		if (!(val & PRNG_STATUS_DATA_AVAIL)) {
+			if (failed++ == PRNG_RETRY_COUNT) {
+				if (currsize == 0)
+					pr_err("Data not available\n");
+				break;
+			}
+			udelay(PRNG_RETRY_DELAY_USEC);
+		} else {
+			val = readl_relaxed(rng->base + PRNG_DATA_OUT);
+			if (!val)
+				break;
 
-		val = readl_relaxed(rng->base + PRNG_DATA_OUT);
-		if (!val)
-			break;
+			*retdata++ = val;
+			currsize += WORD_SZ;
 
-		*retdata++ = val;
-		currsize += WORD_SZ;
-
-		/* make sure we stay on 32bit boundary */
-		if ((maxsize - currsize) < WORD_SZ)
-			break;
+			/* make sure we stay on 32bit boundary */
+			if ((maxsize - currsize) < WORD_SZ)
+				break;
+		}
 	} while (currsize < maxsize);
 
 	clk_disable_unprepare(rng->clk);
+	icc_set(rng->path, 0, rng->min_bw);
+	mutex_unlock(&rng->rng_lock);
 
 	return currsize;
 }
@@ -148,6 +178,26 @@ static int msm_rng_probe(struct platform_device *pdev)
 	if (IS_ERR(rng->clk))
 		return PTR_ERR(rng->clk);
 
+	if (pdev->dev.of_node) {
+		rng->skip_qrng_config = of_property_read_bool(pdev->dev.of_node,
+				"qcom,no-qrng-config");
+		if (of_property_read_u32(pdev->dev.of_node,
+				"qcom,bandwidth-min", &rng->min_bw)) {
+			pr_err("failed to get min bandwidth\n");
+			return -EINVAL;
+		}
+		if (of_property_read_u32(pdev->dev.of_node,
+				"qcom,bandwidth-max", &rng->max_bw)) {
+			pr_err("failed to get max bandwidth\n");
+			return -EINVAL;
+		}
+	}
+
+	rng->path = of_icc_get(&pdev->dev, "cpu");
+	if (IS_ERR(rng->path))
+		return PTR_ERR(rng->path);
+
+	mutex_init(&rng->rng_lock);
 	rng->hwrng.name = KBUILD_MODNAME,
 	rng->hwrng.init = msm_rng_init,
 	rng->hwrng.cleanup = msm_rng_cleanup,
