4db0a94941da950872ee1d8b69d927244a3935e0
Jordan Crouse <jcrouse@codeaurora.org>
2018-10-05 18:38:29 +0530

    INTERNAL: enable System Cache for Graphics memory
    Enable System Cache for Graphics memory.
    
    soc: qcom: llcc-slice: Add error checks for API functions
    
    llcc_slice_getd can return a ERR_PTR code on failure. Add a IS_ERR_OR_NULL
    check to subsequent API calls that use struct llcc_slice_desc to guard
    against faults and to let the leaf drivers get away with safely using a
    ERR_PTR() encoded "pointer" in the aftermath of a llcc_slice_getd error.
    
    iommu/arm-smmu: Add support to use Last level cache
    
    Qualcomm SoCs have an additional level of cache called as
    System cache or Last level cache[1]. This cache sits right
    before the DDR, and is tightly coupled with the memory
    controller.
    The cache is available to all the clients present in the
    SoC system. The clients request their slices from this system
    cache, make it active, and can then start using it. For these
    clients with smmu, to start using the system cache for
    dma buffers and related page tables [2], few of the memory
    attributes need to be set accordingly.
    This change makes the related memory Outer-Shareable, and
    updates the MAIR with necessary protection.
    
    The MAIR attribute requirements are:
        Inner Cacheablity = 0
    	Outer Cacheablity = 1, Write-Back Write Allocate
    	Outer Shareablity = 1
    
    	This change is a realisation of following changes
    	from downstream msm-4.9:
    	iommu: io-pgtable-arm: Support DOMAIN_ATTRIBUTE_USE_UPSTREAM_HINT
    	iommu: io-pgtable-arm: Implement IOMMU_USE_UPSTREAM_HINT
    
    Signed-off-by: Vivek Gautam <vivek.gautam@codeaurora.org>
    
    drm/msm: rearrange the gpu_rmw() function
    
    The register read-modify-write construct is generic enough
    that it can be used by other subsystems as needed, create
    a more generic rmw() function and have the gpu_rmw() use
    this new function.
    
    Reviewed-by: Jordan Crouse <jcrouse@codeaurora.org>
    
    drm/msm/adreno: Add registers in the GPU CX domain
    
    Add the registers needed for configuring the system cache slice info and
    other parameters in the GPU.
    
    arm64:dts:sdm845: Add register range for gpu CX
    
    This patch adds a register range in the gpu CX domain. This is needed to
    support the last level system cache(LLC).
    
    drm/msm: Pass mmu features to generic layers
    
    Allow different Adreno targets the ability to pass
    specific mmu features to the generic layers. This will
    help conditionally configure certain iommu features for
    certain Adreno targets.
    
    Also Add a few simple support functions to support a bitmask of
    features that a specific MMU implementation supports.
    
    drm/msm/a6xx: Add support for using system cache(LLC)
    
    The last level system cache can be partitioned to 32 different slices
    of which GPU has two slices preallocated. One slice is used for caching GPU
    buffers and the other slice is used for caching the GPU SMMU pagetables.
    This patch talks to the core system cache driver to acquire the slice handles,
    configure the SCID's to those slices and activates and deactivates the slices
    upon GPU power collapse and restore.
    
    Some support from the IOMMU driver is also needed to make use of the
    system cache. IOMMU_SYS_CACHE is a buffer protection flag which enables
    caching GPU data buffers in the system cache with memory attributes such
    as outer cacheable, read-allocate, write-allocate for buffers. The GPU
    then has the ability to override a few cacheability parameters which it
    does to override write-allocate to write-no-allocate as the GPU hardware
    does not benefit much from it.
    
    Similarly DOMAIN_ATTR_USE_SYS_CACHE is another domain level attribute
    used by the IOMMU driver to set the right attributes to cache the hardware
    pagetables into the system cache.
    
    Change-Id: Iba172826ebfa9fa6a6d30c255c80e1bc36feef6b
    Signed-off-by: Sharat Masetty <smasetty@codeaurora.org>
    Signed-off-by: Mohammed Thasleem <mthasl@codeaurora.org>

--- drivers/soc/qcom/llcc-slice.c
+++ drivers/soc/qcom/llcc-slice.c
@@ -94,7 +94,8 @@ EXPORT_SYMBOL_GPL(llcc_slice_getd);
  */
 void llcc_slice_putd(struct llcc_slice_desc *desc)
 {
-	kfree(desc);
+	if (!IS_ERR_OR_NULL(desc))
+		kfree(desc);
 }
 EXPORT_SYMBOL_GPL(llcc_slice_putd);
 
@@ -141,6 +142,9 @@ int llcc_slice_activate(struct llcc_slice_desc *desc)
 	int ret;
 	u32 act_ctrl_val;
 
+	if (IS_ERR_OR_NULL(desc))
+		return -EINVAL;
+
 	mutex_lock(&drv_data->lock);
 	if (test_bit(desc->slice_id, drv_data->bitmap)) {
 		mutex_unlock(&drv_data->lock);
@@ -175,6 +179,9 @@ int llcc_slice_deactivate(struct llcc_slice_desc *desc)
 	u32 act_ctrl_val;
 	int ret;
 
+	if (IS_ERR_OR_NULL(desc))
+		return -EINVAL;
+
 	mutex_lock(&drv_data->lock);
 	if (!test_bit(desc->slice_id, drv_data->bitmap)) {
 		mutex_unlock(&drv_data->lock);
@@ -202,6 +209,9 @@ EXPORT_SYMBOL_GPL(llcc_slice_deactivate);
  */
 int llcc_get_slice_id(struct llcc_slice_desc *desc)
 {
+	if (IS_ERR_OR_NULL(desc))
+		return -EINVAL;
+
 	return desc->slice_id;
 }
 EXPORT_SYMBOL_GPL(llcc_get_slice_id);
@@ -212,6 +222,9 @@ EXPORT_SYMBOL_GPL(llcc_get_slice_id);
  */
 size_t llcc_get_slice_size(struct llcc_slice_desc *desc)
 {
+	if (IS_ERR_OR_NULL(desc))
+		return 0;
+
 	return desc->slice_size;
 }
 EXPORT_SYMBOL_GPL(llcc_get_slice_size);
