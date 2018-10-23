fdf78260f94b00b523022c7fd26653f06cf70287
Yanbo Li <yanbol@qca.qualcomm.com>
2018-10-23 11:08:06 +0530

    CHROMIUM: ath10k: Add adjacent wlan radio interference detecting interface
    Some platforms integrate several radios together. When one radio wants to
    spectral scan the current channel, the adjacent radio's activity will
    introduce some interference in the spectral scan result. To combat the
    problem, enable the FW to set a special mark within the spectral scan
    results. Using such a mark, the user who is processing the data can identify
    whether the data has been introduced by the interference of the adjacent radio
    during that period. The user can interpret more accurate spectral scan results
    with the given marks where interference occurred.
    
    This technique requires the preconditions that the scanning and adjacent radio
    have some physical connection to make them aware of each other's activity
    from within the FW layer, and the FW claimed supporting this capability.
    
    to enable this feature, execute:
    echo 1 > /sys/kernel/debug/ieee80211/phyX/ath10k/adjacent_wlan_interfrc
    Then run the spectral scan steps.
    
    To check whether the spectral scan data be interfered, execute:
    
    cat /sys/kernel/debug/ieee80211/phyX/ath10k/adjacent_wlan_interfrc
    
    INTERFRC DETECT FOR SPEC SCAN: Enable/Disable
            5G_interference: X/0
            2G_Interference: X/0
    
    X means the interference number in the scan period, 0 means no interference.
    These counters will be reset when next round spectral scan be triggered
    
    BUG=chrome-os-partner:51964
    TEST=ran iperf after connecting a wifi client with our AP
    
    (cherry picked from commit f3262e6d20cf91ad964b565128a6e16976e01cf2)
    (source: wireless-3.18)
    
    Change-Id: Ie877e4aea6cbd3fb94e5845c97704a15666f6097
    Signed-off-by: Yanbo Li <yanbol@qca.qualcomm.com>
    Signed-off-by: Mohammed Shafi Shajakhan <mohammed@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/340720
    Commit-Ready: Yixiang Li <yixiang@google.com>
    Tested-by: Yixiang Li <yixiang@google.com>
    Reviewed-by: Srinivasa duvvuri <sduvvuri@chromium.org>
    Signed-off-by: Vikas Patel <vikpatel@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -6001,6 +6001,11 @@ static struct sk_buff *ath10k_wmi_10_2_op_gen_init(struct ath10k *ar)
 	if (ath10k_peer_stats_enabled(ar))
 		features |= WMI_10_2_PEER_STATS;
 
+	if ((ar->wlan_interfrc_mask & ATH10K_SPECTRAL_INTERFRC) &&
+	    (test_bit(WMI_SERVICE_AUX_SPECTRAL_INTF, ar->wmi.svc_map)))
+		features |= WMI_10_2_AUX_RADIO_SPECTRAL_INTF;
+
+
 	if (test_bit(WMI_SERVICE_BSS_CHANNEL_INFO_64, ar->wmi.svc_map))
 		features |= WMI_10_2_BSS_CHAN_INFO;
 
