1b54d0f77445ee0a924cbccc83c9632877699a2c
Sean Tranchetti <stranche@codeaurora.org>
2019-01-10 12:30:31 -0700

    rmnet_data: add hooks for rmnet-nss module
    Change-Id: I8317fce05867c8e88ef0d477ede0b6f71981004f
    Signed-off-by: Sean Tranchetti <stranche@codeaurora.org>

---
+++ net/rmnet_data/rmnet_nss.h
@@ -0,0 +1,27 @@
+/* Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _RMNET_NSS_H_
+#define _RMENT_NSS_H_
+
+#include <linux/netdevice.h>
+#include <linux/skbuff.h>
+
+struct rmnet_nss_cb {
+	int (*nss_create)(struct net_device *dev);
+	int (*nss_free)(struct net_device *dev);
+	int (*nss_tx)(struct sk_buff *skb);
+	int (*nss_rx)(struct sk_buff *skb);
+};
+
+extern struct rmnet_nss_cb *rmnet_nss_callbacks;
+
+#endif
