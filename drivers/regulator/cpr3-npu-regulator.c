ee591035e16f7198816ef62b8ea598dacc23045d
Rajith Cherian <rajith@codeaurora.org>
2019-03-19 14:49:35 +0530

    Revert "ipq807x: cpr: No temperature based adjustment for v1 chips"
    This reverts commit 7db68ca4c12246bbdded22f88bad94669cda3d96.
    
    Change-Id: Ia5422349d07db1e96e8b53c2575a0ae1f9891c53
    Signed-off-by: Rajith Cherian <rajith@codeaurora.org>

--- drivers/regulator/cpr3-npu-regulator.c
+++ drivers/regulator/cpr3-npu-regulator.c
@@ -418,7 +418,6 @@ static int cpr3_npu_init_regulator(struct cpr3_regulator *vreg)
 {
 	struct cpr3_ipq807x_npu_fuses *fuse;
 	int rc, cold_temp = 0;
-	bool can_adj_cold_temp = cpr3_can_adjust_cold_temp(vreg);
 
 	rc = cpr3_ipq807x_npu_read_fuse_data(vreg);
 	if (rc) {
@@ -444,13 +443,11 @@ static int cpr3_npu_init_regulator(struct cpr3_regulator *vreg)
 		return rc;
 	}
 
-	if (can_adj_cold_temp) {
-		rc = cpr3_ipq807x_npu_calc_temp_based_ol_voltages(vreg, true);
-		if (rc) {
-			cpr3_err(vreg,
+	rc = cpr3_ipq807x_npu_calc_temp_based_ol_voltages(vreg, true);
+	if (rc) {
+		cpr3_err(vreg,
 			"unable to calculate open-loop voltages, rc=%d\n", rc);
-			return rc;
-		}
+		return rc;
 	}
 
 	rc = cpr3_ipq807x_npu_calc_temp_based_ol_voltages(vreg, false);
@@ -460,19 +457,16 @@ static int cpr3_npu_init_regulator(struct cpr3_regulator *vreg)
 		return rc;
 	}
 
-	if (can_adj_cold_temp) {
-		cpr3_info(vreg,
+	cpr3_info(vreg,
 		"Normal and Cold condition init done. Default to normal.\n");
 
-		rc = cpr3_get_cold_temp_threshold(vreg, &cold_temp);
-		if (rc) {
-			cpr3_err(vreg,
+	rc = cpr3_get_cold_temp_threshold(vreg, &cold_temp);
+	if (rc) {
+		cpr3_err(vreg,
 			"Get cold temperature threshold failed, rc=%d\n", rc);
-			return rc;
-		}
-		register_low_temp_notif(NPU_TSENS, cold_temp,
-							cpr3_npu_temp_notify);
+		return rc;
 	}
+	register_low_temp_notif(NPU_TSENS, cold_temp, cpr3_npu_temp_notify);
 
 	return rc;
 }
