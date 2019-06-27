5fa06a8839d00e7dc43475e82bc14eb9d43b3bba
Rajkumar Ayyasamy <arajkuma@codeaurora.org>
2019-06-27 19:53:21 +0530

    sdhci: msm: Added support to cut down sd card voltage in reboot
    IPQ6018 AP-CP01 board does not have hardware support to cut down the
    voltage in the reboot. So software workaround added to do the same.
    
    Change-Id: Id3edd73df9c0507ff8be37dfaa8bbf2b25786d9a
    Signed-off-by: Rajkumar Ayyasamy <arajkuma@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.h
+++ drivers/mmc/host/sdhci-msm.h
@@ -169,6 +169,8 @@ struct sdhci_msm_host {
 	u32 ice_clk_rate;
 	bool mci_removed;
 	const struct sdhci_msm_offset *offset;
+	int sd_ldo;
+	struct notifier_block reboot_notifier;
 };
 
 extern char *saved_command_line;
