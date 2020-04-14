873d2cb49624d3ffcbbc4872a9b96edb45817aab
Pavithra Palanisamy <pavip@codeaurora.org>
2020-04-14 16:44:47 +0530

    arch: arm: Enable cpufreq driver for IPQ5018
    This change adds support for cpufreq driver with
    'userspace' scaling governor for ipq5018.
    
    Updated CPU operating points in dtsi with
    supported frequencies - 800 MHz and 1 GHz.
    
    Signed-off-by: Pavithra Palanisamy <pavip@codeaurora.org>
    Change-Id: I99a55b8bc07558bc4b027403bc265cb431c8002d

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -13,6 +13,7 @@
 
 static const struct of_device_id machines[] __initconst = {
 	{ .compatible = "qcom,ipq4019" },
+	{ .compatible = "qcom,ipq5018" },
 	{ .compatible = "qcom,ipq807x" },
 
 	{ /* sentinel */ }
