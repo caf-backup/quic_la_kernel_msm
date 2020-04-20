8a581020402136b5b55ae5b538cd69666dfb64bc
Karthick Jeyaraman <kjeyaram@codeaurora.org>
2020-04-20 15:19:38 +0530

    msm: ADSPRPC: Fix to avoid race condition and use after free
    Current mmap and munmap use same mutex but munmap_fd does not
    use the same. This can introduce race condition between mmap
    and munmap_fd. Also there is a use after free scenario in get_args
    and init_process as only mmap_create is protected and the map can
    be freed after this. Unifying mutex to avoid race condition.
    Restricting munmap_fd only for persist bufs to avoid this.
    
    Change-Id: I2adf631b1e61c2274a14a3645a5e555f5d248645
    Acked-by: Ekansh Gupta <ekangupt@qti.qualcomm.com>
    Signed-off-by: Mohammed Nayeem Ur Rahman <mohara@codeaurora.org>
    (cherry picked from commit fd816a905fbe69d048265ef389dcc64b08675367)
    Signed-off-by: Karthick Jeyaraman <kjeyaram@codeaurora.org>
    (cherry picked from commit 61f1103078a8957ae879b36f65eef8d082ddd70e)
    
    Change-Id: Iabcfd40928ce040aede4079be83a9100057fb9b8
    Signed-off-by: Karthick Jeyaraman <kjeyaram@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2699,6 +2699,11 @@ bail:
 	return err;
 }
 
+/*
+ *	fastrpc_internal_munmap_fd can only be used for buffers
+ *	mapped with persist attributes. This can only be called
+ *	once for any persist buffer
+ */
 static int fastrpc_internal_munmap_fd(struct fastrpc_file *fl,
 					struct fastrpc_ioctl_munmap_fd *ud) {
 	int err = 0;
@@ -2716,8 +2721,10 @@ static int fastrpc_internal_munmap_fd(struct fastrpc_file *fl,
 		mutex_unlock(&fl->fl_map_mutex);
 		goto bail;
 	}
-	if (map)
+	if (map && (map->attr & FASTRPC_ATTR_KEEP_MAP)) {
+		map->attr = map->attr & (~FASTRPC_ATTR_KEEP_MAP);
 		fastrpc_mmap_free(map, 0);
+	}
 	mutex_unlock(&fl->fl_map_mutex);
 bail:
 	return err;
