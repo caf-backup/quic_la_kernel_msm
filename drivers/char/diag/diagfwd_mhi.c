MotorolaMobilityLLC/kernel-msm

diag: Do not close mhi channels on usb disconnect
Do not close mhi channels on usb disconnect. Close the
channels only when a ssr notification from MHI occurs.
This will reduce the over head of closing and opening
mhi channels during usb disconnect/connect. Also this
will resolve a deadlock issue while switching the
logging from memory device mode to usb.

Change-Id: If57bc0d4355da1f8e400bab8a312988721157eab
Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>

drivers/char/diag/diagfwd_mhi.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -223,6 +223,8 @@ static int __mhi_close(struct diag_mhi_info *mhi_info, int close_flag)
 	atomic_set(&(mhi_info->read_ch.opened), 0);
 	atomic_set(&(mhi_info->write_ch.opened), 0);
 
+	cancel_work(&mhi_info->read_work);
+	cancel_work(&mhi_info->read_done_work);
 	flush_workqueue(mhi_info->mhi_wq);
 
 	if (close_flag == CLOSE_CHANNELS) {

caf-backup/quic_la_kernel_msm

(cherry picked from commit 549017d90f2c1d2453106a6988de0f6d0c63c0d1)
Signed-off-by: Nikhil Prakash V <nprakash@codeaurora.org>

drivers/char/diag/diagfwd_mhi.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -236,6 +236,8 @@ static int __mhi_close(struct diag_mhi_info *mhi_info, int close_flag)
 	atomic_set(&(mhi_info->read_ch.opened), 0);
 	atomic_set(&(mhi_info->write_ch.opened), 0);
 
+	cancel_work_sync(&mhi_info->read_work);
+	cancel_work_sync(&mhi_info->read_done_work);
 	flush_workqueue(mhi_info->mhi_wq);
 
 	if (close_flag == CLOSE_CHANNELS)