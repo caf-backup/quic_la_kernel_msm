ec655c32423231772a7e8404f7630152d2ae9852
Manikanta Mylavarapu <mmanikan@codeaurora.org>
2020-05-25 09:41:13 +0530

    ipq5018: usb: Fix reference clock config
    This patch will override 19.2MHz
    reference clock configuration
    settings into DWC_USB3_GFLADJ
    and DWC_USB3_GUCTL registers.
    
    Signed-off-by: Manikanta Mylavarapu <mmanikan@codeaurora.org>
    Change-Id: I4eab01755757274d27dc1edd5c5a9a0326d6245d

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -252,6 +252,7 @@
 /* Global Frame Length Adjustment Register */
 #define DWC3_GFLADJ_30MHZ_SDBND_SEL		(1 << 7)
 #define DWC3_GFLADJ_30MHZ_MASK			0x3f
+#define DWC3_GFLADJ_30MHZ_SDBND_SEL_MASK	0x80
 
 /* Device Configuration Register */
 #define DWC3_DCFG_DEVADDR(addr)	((addr) << 3)
