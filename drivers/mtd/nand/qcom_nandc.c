c75e939098146cdd1f00127d44b0398b7e1b62a1
Md Sadre Alam <mdalam@codeaurora.org>
2021-01-19 14:16:09 +0530

    driver: mtd: qpic_nand: Add support for gigadevice GD5F1GQ5REYIH
    Signed-off-by: Md Sadre Alam <mdalam@codeaurora.org>
    Change-Id: I36d1ccadb15e62c18734a1230522a3565cfd685c

--- drivers/mtd/nand/qcom_nandc.c
+++ drivers/mtd/nand/qcom_nandc.c
@@ -396,6 +396,9 @@ struct nand_flash_dev qspinand_flash_ids[] = {
 	{"F50D1G41LB(2M) SPI NAND 1G 1.8V",
 		{ .id = {0xc8, 0x11} },
 		SZ_2K, SZ_128, SZ_128K, 0, 2, 64, NAND_ECC_INFO(4, SZ_512), 0},
+	{"GD5F1GQ5REYIH SPI NAND 1G 1.8V",
+		{ .id = {0xc8, 0x21} },
+		SZ_2K, SZ_128, SZ_128K, 0, 2, 64, NAND_ECC_INFO(4, SZ_512), 0},
 	{NULL}
 };
 
